61c66f1bfb4c43033b1b64fe36cf2ae28b677d80
Toru Kobayashi <koba0004@gmail.com>
2020-01-18 17:43:12 +0900

    feat: implement to get a record value!

--- src/api_client.rs
+++ src/api_client.rs
@@ -1,14 +1,30 @@
+use serde_json::{Value};
+
 pub struct Kintone<'a> {
-    base_url: &'a str
+    base_url: &'a str,
+    api_token: &'a str
 }
 
 impl<'a> Kintone<'a> {
-    pub fn new(base_url: &'a str) -> Kintone {
+    pub fn new(base_url: &'a str, api_token: &'a str) -> Kintone<'a> {
         Kintone {
-            base_url
+            base_url,
+            api_token
         }
     }
     pub fn get_base_url(&self) -> &str {
         self.base_url
     }
+    #[tokio::main]
+    pub async fn get_record(self: &Self, app: i32, record_id: i32) -> Result<Value, Box<dyn std::error::Error>> {
+        let resp = reqwest::Client::new()
+        .get(&format!("{}k/v1/record.json?app={}&id={}", self.base_url, app, record_id))
+        .header("X-Cybozu-API-Token", self.api_token)
+        .send()
+        .await?
+        .json::<Value>()
+        .await?;
+        Ok(resp)
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
+extern crate clap;
+use clap::{Arg, App};
+
 use kintone::api_client::Kintone;
 
 fn main() {
-    let base_url = "https://example.com";
-    let api_client = Kintone::new(base_url);
-    println!("base_url is {}", api_client.get_base_url());
+    let matches = App::new("My Super Program")
+        .version("0.1.0")
+        .author("koba04")
+        .about("API Client for kintone")
+        .arg(
+            Arg::with_name("base_url")
+                .long("base_url")
+                .help("set URL for your kintone environment like https://test.cybozu.com")
+                .takes_value(true)
+        )
+        .arg(
+            Arg::with_name("api_token")
+                .long("api_token")
+                .help("set API Token you want to use")
+                .takes_value(true)
+        )
+        .get_matches()
+    ;
+
+    let base_url = matches.value_of("base_url").expect("base_url is required option");
+    let api_token = matches.value_of("api_token").expect("api_token is required option");
+
+    let api_client = Kintone::new(base_url, api_token);
+    println!("base_url is {}, api_token is {}", api_client.get_base_url(), api_token);
+    let result = api_client.get_record(3, 1).unwrap();
+    println!("record is {}", result["record"]["Text"]["value"]);
 }
