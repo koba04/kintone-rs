d8e9c90d22725925d856e82b8d21b15e39229ac1
Toru Kobayashi <koba0004@gmail.com>
2020-01-26 22:02:43 +0900

    refactor: pass headers to HttpClient

--- src/api_client.rs
+++ src/api_client.rs
@@ -3,27 +3,34 @@ use std::collections::HashMap;
 use crate::http::HttpClient;
 
 pub struct Kintone<'a> {
+    http_client: Box<HttpClient<'a>>,
     base_url: &'a str,
-    api_token: &'a str
 }
 
 impl<'a> Kintone<'a> {
     pub fn new(base_url: &'a str, api_token: &'a str) -> Kintone<'a> {
+        let mut headers = HashMap::new();
+        headers.insert("X-Cybozu-API-Token", api_token);
+        let http_client = Box::new(HttpClient::new(Box::new(headers)));
         Kintone {
             base_url,
-            api_token
+            http_client
         }
     }
     pub fn get_base_url(&self) -> &str {
         self.base_url
     }
     #[tokio::main]
-    pub async fn get_record(self, app: i32, record_id: i32) -> Result<Value, Box<dyn std::error::Error>> {
-        // TODO: create a http client in the constructor
-        let mut headers = HashMap::new();
-        headers.insert(String::from("X-Cybozu-API-Token"), String::from(self.api_token));
-        let http = HttpClient::new(&headers);
-        Ok(http.get(format!("{}k/v1/record.json?app={}&id={}", self.base_url, app, record_id)).await?)
+    pub async fn get_record(&self, app: i32, record_id: i32) -> Result<Value, Box<dyn std::error::Error>> {
+        let res = self.http_client.get(
+            self.build_url(
+                "record.json",
+                &format!("app={}&id={}", app, record_id)
+            )
+        ).await?;
+        Ok(res)
     }
-
+    fn build_url(&self, end_point: &str, query: &str) -> String {
+        format!("{}k/v1/{}?{}", self.base_url, end_point, query)
+    }
 }
--- src/http.rs
+++ src/http.rs
@@ -1,26 +1,24 @@
 use std::collections::HashMap;
 use serde_json::{Value};
 
+type Header<'a> = HashMap<&'a str, &'a str>;
+
 pub struct HttpClient<'a> {
-    headers: &'a HashMap<String, String>
+    headers: Box<Header<'a>>
 }
 
 impl<'a> HttpClient<'a> {
-    pub fn new(headers: &'a HashMap<String, String>) -> HttpClient<'a> {
+    pub fn new(headers: Box<Header<'a>>) -> HttpClient<'a> {
         HttpClient {
             headers
         }
     }
-    pub async fn get(self, url: String) -> Result<Value, Box<dyn std::error::Error>> {
+    pub async fn get(&self, url: String) -> Result<Value, Box<dyn std::error::Error>> {
         let mut client = reqwest::Client::new().get(&url);
-        for (name, value) in self.headers {
-            client = client.header(name, value);
+        for (name, value) in &*self.headers {
+            client = client.header(*name, *value);
         }
-        let resp = client
-        .send()
-        .await?
-        .json::<Value>()
-        .await?;
+        let resp = client.send().await?.json::<Value>().await?;
         // TODO: should not clone
         Ok(resp["record"].clone())
     }
